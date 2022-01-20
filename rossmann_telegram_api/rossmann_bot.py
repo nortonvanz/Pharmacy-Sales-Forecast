cefa3b69b90c495049b1b986d44d0fe93ffbdd92
nortonvanz <nortonmv@gmail.com>
2022-01-19 21:42:28 -0300

    identation fixed

--- rossmann_telegram_api/rossmann_bot.py
+++ rossmann_telegram_api/rossmann_bot.py
@@ -32,7 +32,7 @@ def send_message(chat_id, text):
 
 
 def load_dataset(store_id):
-	
+
 	df10 = pd.read_csv( 'test.csv' )
 	df_store_raw = pd.read_csv( 'store.csv' )
 
@@ -46,17 +46,17 @@ def load_dataset(store_id):
 		df_test = df_test[df_test['Open'] != 0]       #remove closed days
 		df_test = df_test[~df_test['Open'].isnull()]  #pegar linhas que não tem o Open vazio (sem impacto na loja 22..)
 		df_test = df_test.drop( 'Id', axis=1 )        #drop id
-	
+
 		# convert Dataframe to json
 		data = json.dumps( df_test.to_dict( orient='records' ) )
 
-	else: 
+	else:
 		data = 'error'
-	
+
 	return data
 
 def predict (data):
-	 
+
 	# API CALL
 	url = 'https://rossmann-store-prediction.herokuapp.com/rossmann/predict' #ulr heroku + endpoint rossman/predict
 	header = {'Content-type':'application/json'}
@@ -76,11 +76,11 @@ def parse_message( message ):
 	chat_id = message['message']['chat']['id']
 	store_id = message['message']['text']
 
-	store_id = store_id.replace( '/','' )	
+	store_id = store_id.replace( '/','' )
 
 	try:
 		store_id = int( store_id )
-	
+
 	except ValueError:
 		store_id = 'error'
 
@@ -95,18 +95,18 @@ app = Flask( __name__ )
 def index():
 	if request.method == 'POST':
 		message = request.get_json()
-		
+
 		chat_id, store_id = parse_message( message )
-		
+
 		if store_id != 'error':
 			# loading data
 			data = load_dataset( store_id )
 
-			if data != 'error':			
+			if data != 'error':
 
 				# prediction
 				d1 = predict (data )
-		
+
 				# calculation
 				d2 = d1[['store','prediction']].groupby( 'store' ).sum().reset_index()
 
@@ -114,16 +114,16 @@ def index():
 				msg = 'Store Number {} will sell R${:,.2f} in the next 6 weeks'.format(
 					d2['store'].values[0],
 				        d2['prediction'].values[0] )
-				
+
 				send_message( chat_id, msg )
 				return Response( 'Ok', status=200 )
 
-			else:		
+			else:
 				send_message( chat_id, 'Store Not Available!' )
 				return Response( 'Ok, status=200' )
 		else:
-                	send_message( chat_id, 'Store ID is Wrong!' )
-                	return Response( 'Ok, status=200' )
+        	send_message( chat_id, 'Store ID is Wrong!' )
+        	return Response( 'Ok, status=200' )
 
 	else:
 		return '<h1> Rossmann Telegram BOT </h1>'
@@ -131,4 +131,3 @@ def index():
 if __name__ == '__main__':
 	port = os.environ.get( 'PORT', 5000 )
 	app.run( host='0.0.0.0', port=port )
-
